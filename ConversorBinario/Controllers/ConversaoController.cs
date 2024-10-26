925fd44f7c9f43a1b61fc13ab8a0a3fa2ebe96b7
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-10-26 20:15:51 -0300

    Criação do método de conversão de texto para binário

---
+++ ConversorBinario/Controllers/ConversaoController.cs
@@ -0,0 +1,17 @@
+﻿using ConversorBinario.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorBinario.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ConversaoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult TextoParaBinario(string texto)
+    {
+        ConversaoTextoBinario ctb = new ConversaoTextoBinario(texto);
+        var resultado = ctb.ConverterTextoBinario(texto);
+        return Ok(resultado);
+    }
+}
---
+++ ConversorBinario/Domain/Entities/ConversaoTextoBinario.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ConversorBinario.Domain.Entities;
+
+public class ConversaoTextoBinario
+{
+    public string Texto { get; set; }
+
+    public ConversaoTextoBinario(string texto)
+    {
+        Texto = texto;
+    }
+
+    public string ConverterTextoBinario(string texto)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(texto);
+        string resultado = string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+        return resultado;
+    }
+
+}
